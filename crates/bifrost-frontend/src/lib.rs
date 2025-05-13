0de62f290c3db12a509166b098109a5688544482
Christian Iversen <ci@iversenit.dk>
2025-05-13 10:04:55 +0200

    + Add global consts (LazyLock initialized) for BIFROST_SERVER and CLIENT

--- crates/bifrost-frontend/src/lib.rs
+++ crates/bifrost-frontend/src/lib.rs
@@ -5,7 +5,21 @@ pub mod icons;
 pub mod toast;
 pub mod traits;
 
+use std::sync::LazyLock;
+
 use dioxus::prelude::*;
+use reqwest::Url;
+
+use bifrost_api::Client;
+
+static BIFROST_SERVER: LazyLock<String> =
+    LazyLock::new(|| option_env!("BIFROST_SERVER").map_or_else(base_url, ToString::to_string));
+
+static CLIENT: LazyLock<Client> = LazyLock::new(|| {
+    Client::from_url(Url::try_from(format!("{}/bifrost/", *BIFROST_SERVER).as_str()).unwrap())
+});
+
+const LOGO_SVG: Asset = asset!("/assets/logo.svg");
 
 #[must_use]
 pub fn base_url() -> String {
