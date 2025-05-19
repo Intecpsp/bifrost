20e266a820d72ab98874260db9de9cc821ef7ad4
Christian Iversen <ci@iversenit.dk>
2025-05-19 16:24:14 +0200

    + Add support for setting brightness and color temperature for groups

--- crates/bifrost-frontend/src/component/group.rs
+++ crates/bifrost-frontend/src/component/group.rs
@@ -3,16 +3,19 @@ use std::collections::BTreeMap;
 use dioxus::prelude::*;
 use uuid::Uuid;
 
-use hue::api::{Resource, ResourceRecord, Room, Scene, SceneActive, SceneStatus, SceneUpdate};
+use hue::api::{
+    ColorTemperatureUpdate, DimmingUpdate, GroupedLightUpdate, MirekSchema, On, Resource,
+    ResourceRecord, Room, Scene, SceneActive, SceneStatus, SceneUpdate,
+};
 use hue::colorspace::WIDE;
 use hue::colortemp::cct_to_xy;
 use hue::xy::XY;
 
 use crate::HUE_CLIENT;
-use crate::component::colortemp::mirek_to_kelvin;
+use crate::component::brightness::Brightness;
+use crate::component::colortemp::{ColorTemp, mirek_to_kelvin};
 use crate::daisyui::Level;
 use crate::daisyui::badge::Badge;
-use crate::icons::RoomIcon;
 
 fn css_rgb_from_xy(xy: XY) -> String {
     let [x, y, z] = WIDE.xyy_to_xyz(xy.x, xy.y, 0.7);
@@ -21,6 +24,83 @@ fn css_rgb_from_xy(xy: XY) -> String {
     /*     .xy_to_rgb_color(xy.x, xy.y, 100.0) */
     /*     .map(Clamp::unit_to_u8_clamped); */
     /* format!("rgb({}, {}, {})", rgb[0], rgb[1], rgb[2]) */
+}
+
+#[component]
+pub fn GroupOnIcon(id: Uuid, on: On) -> Element {
+    rsx! {
+        input {
+            type: "checkbox",
+            class: "toggle",
+            checked: on.on,
+            onclick: move |evt| {
+                evt.prevent_default();
+
+                let upd = GroupedLightUpdate::new().with_on(Some(On { on: !on.on }));
+
+                async move {
+                    HUE_CLIENT.grouped_light_update(id, upd).await?;
+                    Ok(())
+                }
+            },
+        }
+    }
+}
+
+#[allow(clippy::cast_sign_loss, clippy::cast_possible_truncation)]
+#[component]
+pub fn LightDimming(id: Uuid, dimming: Option<DimmingUpdate>) -> Element {
+    let Some(dim) = &dimming else {
+        return rsx! {};
+    };
+
+    rsx! {
+        Brightness {
+            onchange: move |evt: FormEvent| {
+                evt.prevent_default();
+                evt.stop_propagation();
+                let bri: f64 = evt.value().parse().unwrap();
+                async move {
+                    HUE_CLIENT.grouped_light_update(id, GroupedLightUpdate::new().with_brightness(Some(bri))).await?;
+                    Ok(())
+                }
+            },
+            value: dim.brightness as u32,
+        }
+    }
+}
+
+#[allow(clippy::cast_possible_truncation)]
+#[component]
+pub fn LightColorTemperature(
+    id: Uuid,
+    color_temperature: Option<ColorTemperatureUpdate>,
+) -> Element {
+    let Some(ct) = &color_temperature else {
+        return rsx! {};
+    };
+
+    rsx! {
+        ColorTemp {
+            range: MirekSchema {
+                mirek_minimum: 153,
+                mirek_maximum: 500,
+            },
+            onchange: move |evt: FormEvent| {
+                evt.prevent_default();
+
+                let ct: u16 = evt.value().parse().unwrap();
+                async move {
+                    let upd = GroupedLightUpdate::new()
+                        .with_color_temperature(Some(ct));
+
+                    HUE_CLIENT.grouped_light_update(id, upd).await?;
+                    Ok(())
+                }
+            },
+            value: ct.mirek.map(u32::from),
+        }
+    }
 }
 
 fn css_scene_style(scene: &Scene) -> String {
@@ -69,9 +149,19 @@ pub fn GroupView(res: Signal<BTreeMap<Uuid, ResourceRecord>>, id: Uuid, room: Ro
         .collect();
 
     rsx! {
-        RoomIcon {
-            archetype: room.metadata.archetype,
+        if let Some(gl) = room.grouped_light_service() {
+            if let Some(ResourceRecord {
+                obj: Resource::GroupedLight(grp),
+                ..
+            }) = read.get(&gl.rid) {
+                div {
+                    class: "flex flex-col-2 gap-4",
+                    LightDimming { id: gl.rid, dimming: grp.dimming }
+                    LightColorTemperature { id: gl.rid, color_temperature: grp.color_temperature }
+                }
+            }
         }
+
         ul {
             for child in room.children {
                 li {
